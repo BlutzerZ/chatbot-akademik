2f0f93f3fd2c1b8addd7eb96364010e691ad8f14
BlutzerZ <wafiqnurudin@gmail.com>
2024-10-10 14:37:32 +0700

    add docker and black

--- backend/config/database.py
+++ backend/config/database.py
@@ -8,7 +8,7 @@ env = os.getenv('ENVIRONMENT', 'dev')
 if env == 'prod':
     load_dotenv('.env.prod')
 else:
-    load_dotenv('.env.dev')
+    load_dotenv('.env')
 # Read environment variables
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
@@ -16,7 +16,7 @@ DB_NAME = os.getenv('DB_NAME')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 
 engine = create_engine(DATABASE_URL)
