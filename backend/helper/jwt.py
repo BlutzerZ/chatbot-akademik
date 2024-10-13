aa1e84d37d269fb25e6cfba780baee5b665797e6
BlutzerZ <wafiqnurudin@gmail.com>
2024-10-13 20:36:05 +0700

    modify jwt helper and middleware

--- backend/helper/jwt.py
+++ backend/helper/jwt.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv # type: ignore
-import os, jwt
+import os, jwt, datetime
+
 
 env = os.getenv('ENVIRONMENT', 'prod')
 if env == 'prod':
@@ -8,6 +9,8 @@ else:
     load_dotenv('.env.dev')
 
 def generate_token(data):
+    data['exp'] = datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(minutes=2)
+
     token = jwt.encode(
         data, 
         os.getenv("JWT_SECRET_KEY"), 
@@ -15,3 +18,12 @@ def generate_token(data):
     )
     
     return token
+
+def decode_token(token):
+    data = jwt.decode(
+        token, 
+        os.getenv("JWT_SECRET_KEY"), 
+        algorithms=os.getenv("JWT_ALGORITHM"),
+    )
+    
+    return data
--- backend/middleware/jwt.py
+++ backend/middleware/jwt.py
@@ -1,1 +1,32 @@
-    
+from functools import wraps
+from typing import Annotated, Union
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+import jwt
+from fastapi import Depends, Header, Request, HTTPException
+
+from helper.jwt import decode_token
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        if credentials:
+            if not credentials.scheme == "Bearer":
+                raise HTTPException(status_code=403, detail="Invalid authentication scheme.")
+            payload = self.verify_jwt(credentials.credentials)
+            if payload is None:
+                raise HTTPException(status_code=403, detail="Invalid token or expired token.")
+            request.state.jwtData = payload
+            return credentials.credentials
+        else:
+            raise HTTPException(status_code=403, detail="Invalid authorization code.")
+
+    def verify_jwt(self, jwtoken: str):
+        try:
+            payload = decode_token(jwtoken)
+            return payload 
+        except:
+            return None
